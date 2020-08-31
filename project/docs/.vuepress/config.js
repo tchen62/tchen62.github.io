ad7bd7bc6f0912f2aba741952d59193302623e7a
tchen62 <tchen62@syr.edu>
2020-08-31 00:57:27 -0400

    version1

---
+++ project/docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'Tiehao‘s Blog', 
+    description: 'Just playing around',
+    themeConfig: {
+    nav: [
+      {
+        text: 'Leetcode',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Sql', link: '/leetcode/database/' },
+          { text: 'Algorithm', link: '/leetcode/algorithm/' }
+        ]
+      }
+    ]
+  }
+}
+
+
